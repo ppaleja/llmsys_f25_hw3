6fe80ea7b171154f350731bb7adac6d0f2d591e6
ppaleja <46587956+ppaleja@users.noreply.github.com>
2025-10-09 17:41:10 -0700

    Optimize CUDA compilation flags in compile_cuda.sh for running in Colab

--- compile_cuda.sh
+++ compile_cuda.sh
@@ -1,2 +1,2 @@
 mkdir -p minitorch/cuda_kernels
-nvcc -o minitorch/cuda_kernels/combine.so --shared src/combine.cu -Xcompiler -fPIC
+nvcc -O2 -arch=sm_75 -o minitorch/cuda_kernels/combine.so --shared src/combine.cu -Xcompiler -fPIC
